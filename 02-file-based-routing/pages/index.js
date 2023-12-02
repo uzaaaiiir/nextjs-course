67d0326ac62678af09ee48e69f9c56c7c11433bc
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-12-01 23:51:06 -0500

    NextJs: File-based routing

---
+++ 02-file-based-routing/pages/index.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ 02-file-based-routing/pages/portfolio/[projectid].js
@@ -0,0 +1,18 @@
+// Dynamic route component for portfolio/:id
+
+import { useRouter } from "next/router";
+
+const PortfolioProjectPage = () => {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>Portfolio Project Page</h1>
+    </div>
+  );
+};
+
+export default PortfolioProjectPage;
